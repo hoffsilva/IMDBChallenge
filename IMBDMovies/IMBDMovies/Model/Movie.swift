ee4fdba80006236c2c707dcd79fd872ded0b77c3
Hoff Silva <hoffsilva@macbook-pro-de-hoff.local>
2018-11-30 10:48:09 -0200

    Added pod file.

---
+++ IMBDMovies/IMBDMovies/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  IMBDMovies
+//
+//  Created by Hoff Henry Pereira da Silva on 30/11/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+}
